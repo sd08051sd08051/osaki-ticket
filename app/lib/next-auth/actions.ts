0e80db5c4e62f805f38c90412f80fb914c4abbfc
sd08051sd08051 <southafrica.2018.durban@gmail.com>
2024-03-29 14:41:50 +0900

    Deploy

--- app/lib/next-auth/actions.ts
+++ app/lib/next-auth/actions.ts
@@ -1,6 +1,7 @@
 "use server";
 
-import { signIn } from "@/auth";
+// import { signIn } from "@/auth";
+import { signIn } from "next-auth/react";
 
 export async function authenticateWithGitHub() {
   try {
